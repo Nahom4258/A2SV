aa32077d47dc42584fcb09ab694a7420e7f348ca
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2022-12-21 18:16:04 +0300

    Time: 139 ms (32.72%), Space: 13.9 MB (59.90%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,10 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        ans = True
-        for i, digit in enumerate(str(x)):
-            if digit != str(x)[len(str(x)) - i - 1]:
-                ans = False
-                
-        return ans
+        return str(x) == str(x)[::-1]
             
         
