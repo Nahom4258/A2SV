cb8b62c91355ebb49dc527170bfe78b87a933044
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2022-12-24 00:06:38 +0300

    Time: 296 ms (68.07%), Space: 15.4 MB (48.78%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        # define left & right pointers for the sliding window: these pointers encompase the sides of the riangle
+        left_pointer = 0
+        right_pointer = 2
+        perimeter = 0
+        
+        # sort the given list of triangle sizes
+        nums.sort()
+        
+        # use while loop for sliding window
+        while right_pointer < len(nums):
+            # test if its a triangle using triangle inequalities theorem
+            if (nums[left_pointer] + nums[left_pointer + 1] > nums[right_pointer] and
+                nums[left_pointer] + nums[right_pointer] > nums[left_pointer + 1] and
+                nums[left_pointer + 1] + nums[right_pointer] > nums[left_pointer]):
+                    perimeter = max(perimeter, nums[left_pointer] + nums[left_pointer + 1] + nums[right_pointer])
+            
+            # increment left & right pointer no matter what
+            left_pointer += 1
+            right_pointer += 1
+                    
+        return perimeter
