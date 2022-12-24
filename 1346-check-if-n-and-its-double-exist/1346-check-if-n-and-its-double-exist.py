a07a1489ab512578d0e50f878eb47a68c0b60377
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2022-12-24 12:06:46 +0300

    Time: 69 ms (78.04%), Space: 13.9 MB (89.08%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -4,7 +4,6 @@ class Solution:
         right_pointer = 1
         
         arr.sort()
-        print(arr)
         
         # iterate through using two pointers
         while right_pointer <= len(arr) - 1 and left_pointer <= len(arr) - 2:
