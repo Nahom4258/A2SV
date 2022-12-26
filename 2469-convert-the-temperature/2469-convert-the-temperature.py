601a763d3285e606f3a65cacf99007eb976a8fbf
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2022-12-26 18:02:12 +0300

    Time: 32 ms (89.18%), Space: 13.9 MB (50.93%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -0,0 +1,8 @@
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        ans = []
+        
+        ans.append(celsius + 273.15)
+        ans.append((celsius * (9/5)) + 32)
+        
+        return ans
