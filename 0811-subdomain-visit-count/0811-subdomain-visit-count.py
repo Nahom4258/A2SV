aa603bac5291fdad8df29e7d95974a517eb78a3e
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-01-03 01:01:33 +0300

    Time: 76 ms (61.20%), Space: 14 MB (38.80%) - LeetHub

---
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        _dict = defaultdict(int)
+        
+        for cpdomain in cpdomains:
+            temp = cpdomain.split()
+            num = int(temp[0])
+            first_point = temp[1].find('.') + 1
+            second_point = temp[1].find('.', first_point + 1)
+            # print(num)
+            # print(temp[1][first_point:])
+            # print(temp[1][second_point + 1:])
+            print(temp)
+            if temp[1].count('.') == 2:
+                _dict[temp[1]] += num
+            if temp[1].count('.') >= 1:
+                _dict[temp[1][first_point:]] += num
+            _dict[temp[1][second_point + 1:]] += num
+            
+        # print(_dict)
+        ans = []
+        for key in _dict:
+            ans.append(str(_dict[key]) + ' ' + str(key))
+            
+        print(ans)
+        return ans
