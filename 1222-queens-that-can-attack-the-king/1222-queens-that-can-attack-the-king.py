4dbb6db5acf5ec538c9f5451436991eb2791fc95
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-01-10 18:56:11 +0300

    Time: 47 ms (78.57%), Space: 13.9 MB (53.42%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -1,12 +1,13 @@
 class Solution:
     def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
         answer = []
+        queens = set(map(tuple, queens))
         directions = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,1), (1,0), (1,-1)]
 
         for direction in directions:
             rowK, colK = king[0], king[1]
             while (0 <= rowK < 8) and (0 <= colK < 8):
-                if [rowK, colK] in queens:
+                if (rowK, colK) in queens:
                     answer.append([rowK, colK])
                     break
 
