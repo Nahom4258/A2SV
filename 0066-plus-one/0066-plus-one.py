090456ebeba7aa3a9beb490658ecb50a91fcf3cf
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-01-12 14:34:50 +0300

    Time: 29 ms (95.26%), Space: 13.8 MB (96.79%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,12 +1,14 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         pointer = len(digits) - 1
+        # append variable is added b/c appending inside while loop causes n^2 time complexity
         append = False
         
         while pointer >= 0:
             if digits[pointer] == 9:
                 digits[pointer] = 0
                 if pointer == 0:
+                    # append = True replaces digit.insert(0, 1) to insert outside the while loop
                     append = True
                     break
             else:
