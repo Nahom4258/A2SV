b6f56d2d5fbbe0c04441ff10d50e583d0eb5cdc3
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-01-12 14:31:38 +0300

    Time: 33 ms (86.13%), Space: 13.7 MB (96.79%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,17 +1,21 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         pointer = len(digits) - 1
+        append = False
         
         while pointer >= 0:
             if digits[pointer] == 9:
                 digits[pointer] = 0
                 if pointer == 0:
-                    digits.insert(0, 1)
+                    append = True
                     break
             else:
                 digits[pointer] += 1
                 break
             
             pointer -= 1
+            
+        if append:
+            digits.insert(0, 1)
             
         return digits
