8a97ff2311cc594f2e87033a7d09f18ff345b783
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-01-13 17:37:55 +0300

    Time: 51 ms (65.08%), Space: 14.5 MB (40.80%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,8 +1,11 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # using unique number for each cell
+        # row_index = unique_no // no_of_cols
+        # col_index = uniquie_no % no_of_cols
         
-        for row in range(len(matrix[0]) * len(matrix)):
-            if matrix[row // len(matrix[0])][row % len(matrix[0])] == target:
+        for unique in range(len(matrix[0]) * len(matrix)):
+            if matrix[unique // len(matrix[0])][unique % len(matrix[0])] == target:
                 return True
             
         return False
