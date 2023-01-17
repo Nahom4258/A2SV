0e035a2c3fb1915a3c7b052a3c3240add3e2a931
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-01-17 18:03:33 +0300

    Time: 1758 ms (5.03%), Space: 14.5 MB (42.89%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        left_ptr = 0
+        right_ptr = 1
+        list_size = len(heights)
+        
+        while left_ptr != right_ptr and list_size > 1:
+            if heights[left_ptr] < heights[right_ptr]:
+                heights[left_ptr], heights[right_ptr] = heights[right_ptr], heights[left_ptr]
+                names[left_ptr], names[right_ptr] = names[right_ptr], names[left_ptr]
+            
+            left_ptr += 1
+            right_ptr += 1
+            if right_ptr >= list_size:
+                left_ptr = 0
+                right_ptr = left_ptr + 1
+                list_size -= 1
+                
+        print(heights)
+        print(names)
+        
+        return names
