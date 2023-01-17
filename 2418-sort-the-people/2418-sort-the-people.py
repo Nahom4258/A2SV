b7e2af46a1800ed9417f45795969ab5cc09c6868
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-01-17 18:37:52 +0300

    Time: 616 ms (10.25%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,20 +1,12 @@
-# using bubble sort
+# using selection sort
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        left_ptr = 0
-        right_ptr = 1
-        list_size = len(heights)
-        
-        while left_ptr != right_ptr and list_size > 1:
-            if heights[left_ptr] < heights[right_ptr]:
-                heights[left_ptr], heights[right_ptr] = heights[right_ptr], heights[left_ptr]
-                names[left_ptr], names[right_ptr] = names[right_ptr], names[left_ptr]
+        for i in range(len(heights)):
+            minim = i
+            for j in range(i, len(heights)):
+                if heights[minim] < heights[j]:
+                    minim = j
+            heights[i], heights[minim] = heights[minim], heights[i]
+            names[i], names[minim] = names[minim], names[i]
             
-            left_ptr += 1
-            right_ptr += 1
-            if right_ptr >= list_size:
-                left_ptr = 0
-                right_ptr = left_ptr + 1
-                list_size -= 1
-        
         return names
