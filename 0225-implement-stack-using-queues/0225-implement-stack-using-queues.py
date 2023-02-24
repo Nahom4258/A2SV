383caee11d090c7c48513e0fd48d765497a827e5
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-02-24 10:00:21 +0300

    Time: 19 ms (99.57%), Space: 14.1 MB (17.52%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,58 @@
+class MyStack:
+
+    def __init__(self):
+        self.stack = []
+        self.q2 = []
+
+    def push(self, x: int) -> None:
+        # print('before: ', self.stack)
+        for i in range(len(self.stack)):
+            self.q2.append(self.stack[i])
+        self.stack = []
+            
+        self.stack.append(x)
+        
+        for i in range(len(self.q2)):
+            self.stack.append(self.q2[i])
+        self.q2 = []
+            
+        # print('after: ', self.stack)
+
+    def pop(self) -> int:
+        for i in range(len(self.stack)):
+            self.q2.append(self.stack[i])
+        self.stack = []
+            
+        popped = self.q2[0]
+        del self.q2[0]
+        
+        for i in range(len(self.q2)):
+            self.stack.append(self.q2[i])
+        self.q2 = []
+            
+        return popped
+
+    def top(self) -> int:
+        print('before: ', self.stack)
+        for i in range(len(self.stack)):
+            self.q2.append(self.stack[i])
+        self.stack = []
+            
+        top = self.q2[0]
+        
+        for i in range(len(self.q2)):
+            self.stack.append(self.q2[i])
+        self.q2 = []
+            
+        return top
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
