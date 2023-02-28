94b41ae778a9d92858a45767733f617c43cf8a4e
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-02-28 11:39:51 +0300

    Time: 27 ms (91.33%), Space: 14 MB (5.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 1 or n == 0:
-            return n
+        add = 1
+        left_ptr = 0
+        right_ptr = 1
+        ans = [0,1]
         
-        return self.fib(n - 1) + self.fib(n - 2)
+        if n == 0:
+            return 0
+        
+        for i in range(n-1):
+            ans.append(ans[left_ptr] + ans[right_ptr])
+            left_ptr += 1
+            right_ptr += 1
+            
+        return ans[-1]
