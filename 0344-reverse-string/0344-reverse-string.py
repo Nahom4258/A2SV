86087e5019ae43c56ea3ccc6dc2471109b718e12
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-02-28 15:01:57 +0300

    Time: 211 ms (39.80%), Space: 18.5 MB (37.21%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,4 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        lp = 0
+        rp = len(s) - 1
+        
+        while lp < rp:
+            s[lp], s[rp] = s[rp], s[lp]
+            
+            lp += 1
+            rp -= 1
