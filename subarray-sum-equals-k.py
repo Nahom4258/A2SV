083ccc30a511f6d0066ec47a61c6e25ff6ccc5ae
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-03-01 14:04:10 +0300

    added subarray-sum-equals-k.python

--- subarray-sum-equals-k.py
+++ subarray-sum-equals-k.py
@@ -1,18 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         prefix_sum = 0
-        count = 0
-        dic = dict()
+        counter = 0
+        _dict = defaultdict(int)
+        _dict[0] = 1
 
-        for i in nums:
-            prefix_sum += i
-            if prefix_sum == k:
-                count += 1
-            if dic.get(prefix_sum - k):
-                count += dic.get(prefix_sum - k)
-            if dic.get(prefix_sum):
-                dic[prefix_sum] += 1
-            else:
-                dic[prefix_sum] = 1
+        for val in nums:
+            prefix_sum += val
+            counter += _dict[prefix_sum - k]
+            _dict[prefix_sum] += 1
                 
-        return count
+        return counter
