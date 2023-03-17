3e73fb1c6331f3b25889abd45e3ba6a0066e1271
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-03-17 12:35:34 +0300

    Time: 126 ms (65.39%), Space: 15.3 MB (25.66%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,17 +1,39 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        k = len(p)
-        p = Counter(p)
-        curr = list(s[:k])
+        left_ptr, right_ptr = 0, 0
+        p = dict(Counter(p))
+        new_str = defaultdict(int)
         ans = []
-        
-        if Counter(curr) == p:
-            ans.append(0)
-        
-        for i in range(k, len(s)):
-            curr.append(s[i])
-            del curr[0]
-            if Counter(curr) == p:
-                ans.append(i - k + 1)
-        
+
+        while right_ptr < len(s):
+            # if both pointers are on the same letter
+            if left_ptr == right_ptr:
+                new_str = defaultdict(int)
+                curr = s[right_ptr]
+                # if current letter is in 'p' and needs more to equal 'p'
+                if curr in p and new_str[curr] < p[curr]:
+                    new_str[curr] += 1
+                    if new_str == p:
+                        ans.append(left_ptr)
+                        new_str[curr] -= 1
+                        left_ptr += 1
+                    right_ptr += 1
+                else:
+                    left_ptr += 1
+                    right_ptr += 1
+                continue
+
+            curr = s[right_ptr]
+            # if current letter is in 'p' and needs more to equal 'p'
+            if curr in p and new_str[curr] < p[curr]:
+                new_str[curr] += 1
+                if new_str == p:
+                    ans.append(left_ptr)
+                    new_str[s[left_ptr]] -= 1
+                    left_ptr += 1
+                right_ptr += 1
+            else:
+                new_str[s[left_ptr]] -= 1
+                left_ptr += 1
+
         return ans
