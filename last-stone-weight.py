53308c9a0c658d794cc18677e3a3c9938d9a326b
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-05-04 20:02:01 +0300

    added last-stone-weight.python

--- last-stone-weight.py
+++ last-stone-weight.py
@@ -1,12 +1,13 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-x for x in stones]
+        heapq.heapify(stones)
+
         while len(stones) > 1:
-            heapq._heapify_max(stones)
-            a = heapq._heappop_max(stones)
-            heapq._heapify_max(stones)
-            b = heapq._heappop_max(stones)
+            a = heappop(stones)
+            b = heappop(stones)
 
             if a != b:
-                stones.append(abs(a-b))
+                stones.append(-abs(a-b))
 
-        return stones[0] if stones else 0
+        return -stones[0] if stones else 0
