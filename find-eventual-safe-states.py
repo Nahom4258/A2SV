0333f6c3bc963c8801938db4e2f6dd79f185abd2
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-05-10 19:41:11 +0300

    added find-eventual-safe-states.python

--- find-eventual-safe-states.py
+++ find-eventual-safe-states.py
@@ -1,30 +1,38 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        # 0 - not processed 1 - being processes 2 - already processed
         g = graph
         graph = defaultdict(list)
-        indegree = defaultdict(int)
 
         for i in range(len(g)):
             for child in g[i]:
-                graph[child].append(i)
-                indegree[i] += 1
+                graph[i].append(child)
 
-        queue = deque()
+        # print('ans; ', graph)
+        color = [0] * len(g)
+        def dfs(node):
+            color[node] = 1
+            
+            found = True
+            for neighbour in graph[node]:
+                if color[neighbour] == 1:
+                    return False
+                if color[neighbour] == 0:
+                    found = found and dfs(neighbour)
+
+            if found:
+                color[node] = 2
+
+            return found
 
         for i in range(len(g)):
-            if not indegree[i]:
-                queue.append(i)
+            if color[i] == 0:
+                dfs(i)
 
+        # print('ans: ', color)
         ans = []
+        for i in range(len(color)):
+            if color[i] == 2:
+                ans.append(i)
 
-        while queue:
-            curr = queue.popleft()
-            ans.append(curr)
-
-            for child in graph[curr]:
-                indegree[child] -= 1
-
-                if not indegree[child]:
-                    queue.append(child)
-
-        return sorted(ans)
+        return ans
