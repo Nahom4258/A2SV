f37676d73ad2e9e8f512639b92084a187a5171ee
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-06-15 21:08:44 +0300

    added champagne-tower.python

---
+++ champagne-tower.py
@@ -0,0 +1,18 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        memo = defaultdict(int)
+        memo[(0, 0)] = poured
+
+        for i in range(100):
+            for j in range(i+1):
+                if memo[(i, j)] > 1:
+                    left_over = memo[(i, j)] - 1
+                    memo[(i, j)] -= left_over
+                    # add to left
+                    memo[(i+1, j)] += left_over/2
+                    # add to right
+                    memo[(i+1, j+1)] += left_over/2
+                    
+        # print('ans: ', memo)
+
+        return memo[(query_row, query_glass)]
