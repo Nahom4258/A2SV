798eb4afc7b1c846a71d83703a499b278f31d6a3
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-06-15 21:19:19 +0300

    added champagne-tower.python

--- champagne-tower.py
+++ champagne-tower.py
@@ -3,7 +3,7 @@ class Solution:
         memo = defaultdict(int)
         memo[(0, 0)] = poured
 
-        for i in range(100):
+        for i in range(query_row+1):
             for j in range(i+1):
                 if memo[(i, j)] > 1:
                     left_over = memo[(i, j)] - 1
