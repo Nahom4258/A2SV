41e6411225fd41005ccec7e9d5452e8407810d2d
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-06-20 01:12:27 +0300

    added first-unique-character-in-a-string.python

---
+++ first-unique-character-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counter = Counter(s)
+
+        for index, char in enumerate(s):
+            if counter[char] == 1:
+                return index
+
+        return -1
