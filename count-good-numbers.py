c02ca9adc9311909c5e13b9026e5102707df47ae
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-10-25 14:23:02 +0300

    added count-good-numbers.python

--- count-good-numbers.py
+++ count-good-numbers.py
@@ -1,20 +1,22 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
 
-        def mod(x, n, mod):
-            result = 1
-            x = x % mod
-            while n > 0:
-                if n % 2 == 1:
-                    result = (result * x) % mod
-                x = (x * x) % mod
-                n = n // 2
-            return result
+        def mod(x, n, m):
+            if n == 0:
+                return 1
+            if n == 1:
+                return x
+
+            if n % 2 == 0:
+                return mod((x*x) % m, n//2, m)
+            else:
+                return x * mod((x*x) % m, (n-1)//2, m)
+
 
         MOD = 10**9 + 7
 
         odd_no = n//2
-        even_no = odd_no + (0 if n%2==0 else 1)
+        even_no = odd_no + n%2
 
         evens = mod(5, even_no, MOD)
         primes = mod(4, odd_no, MOD)
