4a380d83278c0bbbf835177da3b79e2dd8d7fe6c
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2023-10-27 21:42:09 +0300

    Added solution for Two Sum

--- internal_interviews/two-sum.py
+++ internal_interviews/two-sum.py
@@ -1,9 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = defaultdict(list)
         n = len(nums)
+
         for i in range(n):
-            for j in range(n):
-                if i != j and nums[i] + nums[j] == target:
-                    return [i, j]
+            num = nums[i]
+            if num not in indices:
+                indices[num].append(i)
+            else:
+                if len(indices[num]) < 2 and i not in indices[num]:
+                    indices[num].append(i)
+
+        nums.sort()
+        l, r = 0, n-1
+        while l < r:
+            add = nums[l] + nums[r]
+            if add == target:
+                if nums[l] == nums[r]:
+                    return indices[nums[l]]
+
+                return [indices[nums[l]][0], indices[nums[r]][0]]
+            elif add < target: 
+                l += 1
+            else:
+                r -= 1
 
         return []
