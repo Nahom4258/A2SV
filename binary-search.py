c1932350061b2f8d3ca2132495de0e4a1f4b39b3
Nahom Habtamu <70732076+Nahom4258@users.noreply.github.com>
2025-03-04 20:54:16 +0300

    added binary-search.python

--- binary-search.py
+++ binary-search.py
@@ -1,15 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start = 0
-        end = len(nums) - 1
-        
-        while start <= end:
-            mid = (start + end) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                start = mid + 1
+        # Use binary search
+        # Sort the list
+        # For each search iteration, we reduce the list by half, discarding the other half as not containing the target value
+
+        nums.sort()
+
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            ind = (r+l)//2
+
+            if nums[ind] < target:
+                l = ind+1
+            elif nums[ind] > target:
+                r = ind-1
             else:
-                end = mid - 1
-                
+                return ind
+
         return -1
